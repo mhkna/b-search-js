44989c65786be51f02151814ef2bc40eae472415
Michael <michkna3@gmail.com>
2017-10-07 00:26:59 -0500

    search helper methods created and beginning if else logic started for main function

--- binary.js
+++ binary.js
@@ -1,7 +1,23 @@
-const bSearch = (val, arr, min, max) => {
+arrayay = [1, 4, 9, 19, 45, 62, 66, 78, 93]
 
+
+const bSearch = (val, arr, min, max) => {
+  mid = midVal(arr)
+  if (mid < val) {
+
+  } else if (mid > val) {
+
+  } else {
+    return mid;
+  }
 };
 
-const midPoint = (arr) => {
+const midIndex = (arr) => {
   return Math.floor(arr.length / 2)
 };
+
+const midVal = (arr) => {
+  return arr[midIndex(arr)]
+};
+
+bSearch(4, arrayay, 1, 4)
