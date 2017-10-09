a5f9595d64caf8298addeda9e94bb5c658b2b964
Michael <michkna3@gmail.com>
2017-10-09 15:10:18 -0500

    added ruby solution

--- binary.js
+++ binary.js
@@ -20,4 +20,27 @@ const midVal = (arr) => {
   return arr[midIndex(arr)]
 };
 
-bSearch(4, arrayay, 1, 4)
+bSearch(4, arrayay, min, max)
+
+
+
+
+/*
+arrayay = [1, 4, 9, 19, 45, 62, 66, 78, 93, 99]
+
+def bSearch(val, arr, min = 0, max = arr.length - 1)
+  unless arr.include?(val)
+    return "NOT FOUND"
+  end
+  mid_index = (max + min) / 2
+  if val < arr[mid_index]
+    bSearch(val, arr, min, mid_index - 1)
+  elsif val > arr[mid_index]
+    bSearch(val, arr, mid_index + 1, max)
+  else
+    return mid_index
+  end
+end
+
+p bSearch(3, arrayay)
+*/
