b808cbac6c532f12e205af867db4619010e7c1f4
Michael <michkna3@gmail.com>
2018-04-24 16:04:13 -0400

    1.5 can be mult methods

--- ctci/ch1/1.5.rb
+++ ctci/ch1/1.5.rb
@@ -1,7 +1,6 @@
 #edit --> insert, remove, or replace
 #check if 1 or 0 edits away
 
-
 #oh dear
 def one_away?(a, b)
   if a.length >= b.length
@@ -38,4 +37,49 @@ def one_away?(a, b)
   return false if a_arr.length > 1
   return true
 
+end
+
+
+
+#book answer
+def one_edit_away(first, second)
+  if first.length == second.length
+    return one_edit_replace(first, second)
+  elsif first.length + 1 == second.length
+    return one_edit_insert(first, second)
+  elsif first.length - 1 == second.length
+    return one_edit_insert(second, first)
+  end
+  return false
+end
+
+def one_edit_replace(s1, s2)
+  found_dif = false
+  s1.length.times do |i|
+    if s1[i] != s2[i]
+      if found_dif
+        return false
+      end
+      found_dif = true
+    end
+    i += 1
+  end
+  return true
+end
+
+def one_edit_insert(s1, s2)
+  index1 = 0
+  index2 = 0
+  while index2 < s2.length && index1 < s1.length
+    if s1[index1] != s2[index2]
+      if index1 != index2
+        return false
+      end
+      index2 += 1
+    else
+      index1 += 1
+      index2 += 1
+    end
+  end
+  return true
 end
